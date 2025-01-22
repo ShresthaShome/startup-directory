f9ec2eab1e4352a09ea3cd46b26ff12763828fab
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-01-22 19:15:05 +0600

    feat: Startup Card
    And Next is configured to handle image from all source.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: 1, name: "BOSS Ullas" },
+      _id: 1,
+      description: "description",
+      image: "https://i1.perfumesclub.com/grande/23339-4.jpg",
+      category: "BOSS",
+      title: "BOSS",
+    },
+  ];
 
   return (
     <>
@@ -15,10 +29,26 @@ export default async function Home({
         </h1>
 
         <p className="sub-heading !max-w-3xl ">
-          Submit ideas, vote on pitches, get noticed and compete virtually!
+          Submit Ideas, Vote on Pitches, Get Noticed and Compete Virtually!
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold ">
+          {query ? `All search results for "${query}"` : "Showing All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length ? (
+            posts.map((post /*: StartupCardType*/, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );

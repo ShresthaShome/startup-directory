b2649f4e5c9c0a6ae52b69e8bf346356f167df5f
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-01-22 15:22:19 +0600

    feat: Hero section with added fonts, and configured shadcn
    Added fonts and integrated shadcn for styling. Updated global.css to reflect new changes and added a hero section to page.tsx.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Startup, <br /> Connect With Other Entreprenuers
+        </h1>
+
+        <p className="sub-heading !max-w-3xl ">
+          Submit ideas, vote on pitches, get noticed and compete virtually!
+        </p>
+      </section>
     </>
   );
 }

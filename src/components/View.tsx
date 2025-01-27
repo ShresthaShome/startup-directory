ad5cc61e02f705d34c60bcfe0d2ebf769d54ec87
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-01-27 23:36:32 +0600

    feat: writeClient added
    Views now get updated automatically and catching has been turned on.

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,13 +1,16 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 export default async function View({ id }: { id: string }) {
-  const { views } = await client
-    .withConfig({ useCdn: false })
-    .fetch(STARTUP_VIEWS_QUERY, { id });
+  const { views }: { views: number } =
+    (await client
+      .withConfig({ useCdn: false })
+      .fetch(STARTUP_VIEWS_QUERY, { id })) || 0;
 
-  // TODO: Update when viewed
+  after(async () => await writeClient.patch(id).inc({ views: 1 }).commit());
 
   return (
     <div className="view-container">

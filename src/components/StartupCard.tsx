7e6cf433a6f5d8837f6dbbcc2443f08c8202e928
Shrestha Shome Ullas <shrestha.shome@gmail.com>
2025-01-29 07:34:33 +0600

    style: Final styling done.
    Ready for built.

--- src/components/StartupCard.tsx
+++ src/components/StartupCard.tsx
@@ -32,13 +32,13 @@ export default function StartupCard({ post }: { post: StartupCardType }) {
         )) || <b className="text-black mr-1">NEW</b>}
       </div>
 
-      <div className="flex-between mt-5 gap-5">
+      <div className="flex-between mt-5 gap-5 relative">
         <div className="flex-1">
           <Link href={`/user/${author?._id}`}>
             <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
-            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+            <h3 className="text-26-semibold line-clamp-3">{title}</h3>
           </Link>
         </div>
         <Link href={`/user/${author?._id}`}>
@@ -47,7 +47,7 @@ export default function StartupCard({ post }: { post: StartupCardType }) {
             width={40}
             height={40}
             alt="placeholder"
-            className="rounded-full"
+            className="rounded-full min-w-10 min-h-10 absolute -right-4 top-3"
           />
         </Link>
       </div>
